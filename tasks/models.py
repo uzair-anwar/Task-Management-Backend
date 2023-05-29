02c67989c27f10278aee62d891055696772e179a
Uzair Anwar <uzair.anwar@devsinc.com>
2023-05-29 16:42:48 +0500

    Added model for task

---
+++ tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    due_date = models.DateField()
+    assignee = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='assigned_tasks')
+    creator = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='created_tasks')
+    status = models.CharField(max_length=20, choices=[(
+        'todo', 'To Do'), ('in_progress', 'In Progress'), ('completed', 'Completed')], default='todo')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
