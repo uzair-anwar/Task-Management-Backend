7024a7ede9d2911d9d0d7a99ff60b0de91c643a3
Uzair Anwar <uzair.anwar@devsinc.com>
2023-05-29 14:41:58 +0500

    Changed database configration and apply migrations

--- taskmanagement_backend/settings.py
+++ taskmanagement_backend/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "taskmanagement_backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskmanagement',
+        'USER': 'postgres',
+        'PASSWORD': 'Uzair6702',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
