81f2cb301f094c82e8cba50ad9737acb1fe05d88
Uzair Anwar <uzair.anwar@devsinc.com>
2023-05-29 17:08:17 +0500

    Added serializers, views and urls for tasks model

--- taskmanagement_backend/settings.py
+++ taskmanagement_backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
-    "users"
+    "users",
+    "tasks"
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Task.objects.all()
+        else:
+            return Task.objects.filter(assignee=user)
