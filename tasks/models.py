722f2ab08448cb25e839c9d7761481302468d07d
Uzair Anwar <uzair.anwar@devsinc.com>
2023-05-29 17:42:42 +0500

    Added update task assignee or status views

--- tasks/models.py
+++ tasks/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth import get_user_model
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=25)
-    description = models.TextField()
-    due_date = models.DateField()
+    title = models.CharField(max_length=25, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
     assignee = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name='assigned_tasks')
     creator = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, related_name='created_tasks')
+        get_user_model(), on_delete=models.CASCADE, related_name='created_tasks', blank=True, null=True)
     status = models.CharField(max_length=20, choices=[(
         'todo', 'To Do'), ('in_progress', 'In Progress'), ('completed', 'Completed')], default='todo')
     created_at = models.DateTimeField(auto_now_add=True)
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from users.serializers import CustomUserSerializer
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -14,3 +18,24 @@ class TaskViewSet(viewsets.ModelViewSet):
             return Task.objects.all()
         else:
             return Task.objects.filter(assignee=user)
+
+
+def update(self, request, *args, **kwargs):
+    instance = self.get_object()
+    assignee_id = request.data.get('assignee')
+    if assignee_id:
+        try:
+            assignee = CustomUserSerializer.objects.get(id=assignee_id)
+            instance.assignee = assignee
+            instance.save()
+            return Response({'status': 'Task assigned successfully.'})
+        except CustomUserSerializer.DoesNotExist:
+            return Response({'error': 'Invalid assignee ID.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    status = request.data.get('status')
+    if status:
+        instance.status = status
+        instance.save()
+        return Response({'status': 'Task status updated successfully.'})
+
+    return Response({'error': 'Status field is required.'}, status=status.HTTP_400_BAD_REQUEST)
