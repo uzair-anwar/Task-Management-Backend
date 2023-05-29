2a8ac258da9fa7a80dda119f95388f362c210316
Uzair Anwar <uzair.anwar@devsinc.com>
2023-05-29 16:13:04 +0500

    Added views and urls to get mangers and regular users

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
-from .views import RegisterUserView, LoginUserView
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework import routers
+
+from .views import RegisterUserView, LoginUserView
 from .views import CustomTokenObtainPairView
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'managers', UserViewSet, basename='managers')
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
+from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .serializers import CustomTokenObtainPairSerializer
+from .serializers import CustomUserSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
@@ -18,3 +21,18 @@ class LoginUserView(TokenObtainPairView):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.role == 'admin':
+            return get_user_model().objects.filter(role='manager')
+        if self.request.user.role == 'manager':
+            return get_user_model().objects.filter(role='regular')
+        else:
+            return get_user_model().objects.none()
+
+    def get_serializer_class(self):
+        return CustomUserSerializer
